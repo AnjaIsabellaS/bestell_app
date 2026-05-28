b59cf1484738c3b85f875a11910e10659f0cce1b
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-05-28 17:36:32 +0200

    function init and function renderAllMenus added

--- assets/styles/scripts/db.js
+++ assets/styles/scripts/db.js
@@ -3,28 +3,28 @@ const menus = [
     "name": "Veggie mushroom black burger",
     "price": 16.90,   
     "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",  
-    "category": "Burger & Sandwiches",  
+    "category": "Burger",  
     "image": "./assets/img/veggie_mushroom_black_burger.png"
     },
     {
     "name": "All meat burger",
     "price": 15.90,   
     "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",  
-    "category": "Burger & Sandwiches", 
+    "category": "Burger", 
     "image": "./assets/img/all_meat_burger.png"
     },
     {
     "name": "Beef red burger",
     "price": 14.90,   
     "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",  
-    "category": "Burger & Sandwiches",
+    "category": "Burger",
     "image": "./assets/img/beef_red_burger.png"
     },
     {
     "name": "BIg chicken burger",
     "price": 15.90,   
     "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",  
-    "category": "Burger & Sandwiches",
+    "category": "Burger",
     "image": "./assets/img/big_chicken_burger.png"
     },
     {
---
+++ script.js
@@ -0,0 +1,25 @@
+function init() { 
+    renderAllMenus(); 
+}
+
+function renderAllMenus() {
+    const burgerRef = document.getElementById('burger-sandwiches');
+    const pizzaRef = document.getElementById('pizza');
+    const saladRef = document.getElementById('salad');
+
+    burgerRef.innerHTML = "";
+    pizzaRef.innerHTML = "";
+    saladRef.innerHTML = "";
+
+    for (let index = 0; index < menus.length; index++) {
+        const item = menus[index];
+
+        if (item.category === "Burger") {
+            burgerRef.innerHTML += getMenuTemplate(index);
+        } else if (item.category === "Pizza") {                 // Nutzt das gleiche Template-System
+            pizzaRef.innerHTML += getMenuTemplate(index); 
+        } else if (item.category === "Salad") {
+            saladRef.innerHTML += getMenuTemplate(index);
+        }
+    }
+}
