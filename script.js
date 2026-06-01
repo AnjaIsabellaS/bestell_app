0aeebc9862243c6d24d67d0baf5f98f839e6b318
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-06-01 08:29:28 +0200

    add to cart function changed

--- script.js
+++ script.js
@@ -1,89 +1,93 @@
 const menus = [
     {
-    "name": "Veggie mushroom black burger",
-    "price": 16.90,   
-    "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",  
-    "category": "Burger",  
-    "image": "./assets/img/veggie_mushroom_black_burger.png"
+        "name": "Veggie mushroom black burger",
+        "price": 16.90,   
+        "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",  
+        "category": "Burger",  
+        "image": "./assets/img/veggie_mushroom_black_burger.png"
     },
     {
-    "name": "All meat burger",
-    "price": 15.90,   
-    "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",  
-    "category": "Burger", 
-    "image": "./assets/img/all_meat_burger.png"
+        "name": "All meat burger",
+        "price": 15.90,   
+        "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",  
+        "category": "Burger", 
+        "image": "./assets/img/all_meat_burger.png"
     },
     {
-    "name": "Beef red burger",
-    "price": 14.90,   
-    "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",  
-    "category": "Burger",
-    "image": "./assets/img/beef_red_burger.png"
+        "name": "Beef red burger",
+        "price": 14.90,   
+        "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",  
+        "category": "Burger",
+        "image": "./assets/img/beef_red_burger.png"
     },
     {
-    "name": "BIg chicken burger",
-    "price": 15.90,   
-    "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",  
-    "category": "Burger",
-    "image": "./assets/img/big_chicken_burger.png"
+        "name": "BIg chicken burger",
+        "price": 15.90,   
+        "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",  
+        "category": "Burger",
+        "image": "./assets/img/big_chicken_burger.png"
     },
     {
-    "name": "Pizza Margherita",
-    "price": 11.90,   
-    "description": "Tomato slices, Mozzarella, Chorizo",  
-    "category": "Pizza",
-    "image": "./assets/img/pizza_margherita.png"
+        "name": "Pizza Margherita",
+        "price": 11.90,   
+        "description": "Tomato slices, Mozzarella, Chorizo",  
+        "category": "Pizza",
+        "image": "./assets/img/pizza_margherita.png"
     },
     {
-    "name": "Pizza Chorizo",
-    "price": 13.90,   
-    "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",  
-    "category": "Pizza",
-    "image": "./assets/img/pizza_chorizo.png"
+        "name": "Pizza Chorizo",
+        "price": 13.90,   
+        "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",  
+        "category": "Pizza",
+        "image": "./assets/img/pizza_chorizo.png"
     },
     {
-    "name": "Funghi",
-    "price": 12.90,   
-    "description": "Red onion, Olives, Button Mushrooms, Mozzarella",  
-    "category": "Pizza",
-    "image": "./assets/img/pizza_funghi.png"
+        "name": "Funghi",
+        "price": 12.90,   
+        "description": "Red onion, Olives, Button Mushrooms, Mozzarella",  
+        "category": "Pizza",
+        "image": "./assets/img/pizza_funghi.png"
     },
     {
-    "name": "Quattro Formaggi with Chicken",
-    "price": 15.90,   
-    "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",  
-    "category": "Pizza",
-    "image": "./assets/img/pizza_quattro_formaggi_chicken.png"
-    },
-      {
-    "name": "Warm beef arugula salad",
-    "price": 16.90,   
-    "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",  
-    "category": "Salad",
-    "image": "./assets/img/warm_beef_arugula_salad.png" 
+        "name": "Quattro Formaggi with Chicken",
+        "price": 15.90,   
+        "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",  
+        "category": "Pizza",
+        "image": "./assets/img/pizza_quattro_formaggi_chicken.png"
     },
     {
-    "name": "Mini green Salad",
-    "price": 7.90,   
-    "description": "Green salad, Cucumber, Carrots, Parsley, Radishes ",  
-    "category": "Salad",
-    "image": "./assets/img/mini_green_salad.png" 
+        "name": "Warm beef arugula salad",
+        "price": 16.90,   
+        "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",  
+        "category": "Salad",
+        "image": "./assets/img/warm_beef_arugula_salad.png" 
     },
     {
-    "name": "Green Salad with sea food",
-    "price": 16.90,   
-    "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",  
-    "category": "Salad",
-    "image": "./assets/img/green_salad_sea_food.png"
+        "name": "Mini green Salad",
+        "price": 7.90,   
+        "description": "Green salad, Cucumber, Carrots, Parsley, Radishes ",  
+        "category": "Salad",
+        "image": "./assets/img/mini_green_salad.png" 
     },
     {
-    "name": "Vegan green salad with tofu",
-    "price": 14.90,   
-    "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",  
-    "category": "Salad",
-    "image": "./assets/img/vegan_green_salad_tofu.png"
+        "name": "Green Salad with sea food",
+        "price": 16.90,   
+        "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",  
+        "category": "Salad",
+        "image": "./assets/img/green_salad_sea_food.png"
     },
-]
+    {
+        "name": "Vegan green salad with tofu",
+        "price": 14.90,   
+        "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",  
+        "category": "Salad",
+        "image": "./assets/img/vegan_green_salad_tofu.png"
+    }
+];
+
+
+const basket = [];
+
 
 function init() { 
     renderAllMenus(); 
@@ -124,7 +128,131 @@ function getMenuTemplate(index) {
             <p class="menu-description">${menu.description}</p>
         </div>
         <div class="menu-price">${menu.price.toFixed(2).replace('.', ',')}€</div>
-        <button class="add-basket-btn">Add to basket</button>
+        <button class="add-basket-btn" onclick="addToBasket(${index})">Add to basket</button>
      </div>
     `;
+}
+
+
+function getMenuIndex(menuName) {
+    let namesOnly = [];
+    
+    for (let i = 0; i < basket.length; i++) {
+        namesOnly.push(basket[i].name);
+    }
+    return namesOnly.indexOf(menuName);
+}
+
+
+function addToBasket(index) {
+    const selectedMenu = menus[index];
+    const menuIndex = getMenuIndex(selectedMenu.name);
+
+    if (menuIndex === -1) {
+        basket.push({
+            name: selectedMenu.name,
+            price: selectedMenu.price,
+            amount: 1
+        });
+    } else {
+        basket[menuIndex].amount++;
+    }
+
+    const basketRef = document.getElementById('basket');
+    basketRef.classList.remove('d-none');
+    
+    renderBasket();
+}
+
+function renderBasket() {
+    const basketRef = document.getElementById('basket');
+    
+    if (basket.length === 0) {
+        basketRef.classList.add('d-none');
+    } else {
+        const prices = calculateBasketPrices();
+        basketRef.innerHTML = getMainBasketTemplate(prices);
+    }
+}
+
+function calculateBasketPrices() {
+    let subtotal = 0;
+    let deliveryFee = 4.99;
+    let itemsHtml = "";
+
+    for (let i = 0; i < basket.length; i++) {
+        subtotal += basket[i].price * basket[i].amount; 
+        itemsHtml += getBasketItemTemplate(i);
+    }
+
+    let total = subtotal + deliveryFee;
+
+    return {
+        subtotal: subtotal,
+        deliveryFee: deliveryFee,
+        total: total,
+        itemsHtml: itemsHtml
+    };
+}
+
+function getMainBasketTemplate(prices) {
+    return `
+        <h3>Your Basket</h3>
+        
+        <div id="basket-items">
+            ${prices.itemsHtml}
+        </div>
+        
+        <div class="basket-summary">
+            <div class="summary-row"><span>Subtotal</span><span>${prices.subtotal.toFixed(2).replace('.', ',')}€</span></div>
+            <div class="summary-row"><span>Delivery fee</span><span>${prices.deliveryFee.toFixed(2).replace('.', ',')}€</span></div>
+            <hr class="summary-divider">
+            <div class="summary-row total-row"><span>Total</span><span>${prices.total.toFixed(2).replace('.', ',')}€</span></div>
+        </div>
+        
+        <button class="buy-now-btn" id="basket-btn">Buy now (${prices.total.toFixed(2).replace('.', ',')}€)</button>
+    `;
+}
+
+function getBasketItemTemplate(i) {
+    const item = basket[i];
+    const totalItemPrice = item.price * item.amount;
+    
+    return `
+        <div class="basket-item-card" style="margin-bottom: 12px;">
+            <h5>${item.amount}x ${item.name}</h5>
+            <div class="basket-item-controls">
+                <div class="amount-buttons">
+                    <button class="ctrl-btn delete-btn" onclick="deleteFromBasket(${i})">
+                        <img src="./assets/icons/delete.png" alt="Löschen" class="icon-img">
+                    </button>
+                    
+                    <button class="ctrl-btn" onclick="decreaseAmount(${i})" ${item.amount <= 1 ? 'disabled' : ''}>−</button>
+                    
+                    <button class="ctrl-btn" onclick="increaseAmount(${i})">+</button>
+                </div>
+                <div class="basket-item-price">${totalItemPrice.toFixed(2).replace('.', ',')}€</div>
+            </div>
+        </div>
+    `;
+}
+
+
+function increaseAmount(i) {
+    basket[i].amount++;
+    renderBasket();
+}
+
+
+function decreaseAmount(i) {
+    if (basket[i].amount > 1) {
+        basket[i].amount--;
+        renderBasket();
+    }
+}
+
+
+function deleteFromBasket(i) {
+    basket.splice(i, 1);
+    renderBasket();
 }
