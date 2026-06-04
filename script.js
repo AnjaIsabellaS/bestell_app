08086231e7c99a9d4cef54ee33f8176985e72850
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-06-04 12:03:44 +0200

    mobile images ad array added

--- script.js
+++ script.js
@@ -4,84 +4,96 @@ const menus = [
         "price": 16.90,   
         "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",  
         "category": "Burger",  
-        "image": "./assets/img/veggie_mushroom_black_burger.png"
+        "image": "./assets/img/veggie_mushroom_black_burger.png",
+        "imageMobile": "./assets/img/veggie_mushroom_black_mobile.png"
     },
     {
         "name": "All meat burger",
         "price": 15.90,   
         "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",  
         "category": "Burger", 
-        "image": "./assets/img/all_meat_burger.png"
+        "image": "./assets/img/all_meat_burger.png",
+        "imageMobile": "./assets/img/all_meat_burger_mobile.png"
     },
     {
         "name": "Beef red burger",
         "price": 14.90,   
         "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",  
         "category": "Burger",
-        "image": "./assets/img/beef_red_burger.png"
+        "image": "./assets/img/beef_red_burger.png",
+        "imageMobile": "./assets/img/beef_red_burger_mobile.png"
     },
     {
         "name": "BIg chicken burger",
         "price": 15.90,   
         "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",  
         "category": "Burger",
-        "image": "./assets/img/big_chicken_burger.png"
+        "image": "./assets/img/big_chicken_burger.png",
+        "imageMobile": "./assets/img/big_chicken_burger_mobile.png"
     },
     {
         "name": "Pizza Margherita",
         "price": 11.90,   
         "description": "Tomato slices, Mozzarella, Chorizo",  
         "category": "Pizza",
-        "image": "./assets/img/pizza_margherita.png"
+        "image": "./assets/img/pizza_margherita.png",
+        "imageMobile": "./assets/img/pizza_margherita_mobile.png"
     },
     {
         "name": "Pizza Chorizo",
         "price": 13.90,   
         "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",  
         "category": "Pizza",
-        "image": "./assets/img/pizza_chorizo.png"
+        "image": "./assets/img/pizza_chorizo.png",
+        "imageMobile": "./assets/img/pizza_chorizo_mobile.png"
     },
     {
         "name": "Funghi",
         "price": 12.90,   
         "description": "Red onion, Olives, Button Mushrooms, Mozzarella",  
         "category": "Pizza",
-        "image": "./assets/img/pizza_funghi.png"
+        "image": "./assets/img/pizza_funghi.png",
+        "imageMobile": "./assets/img/pizza_funghi_mobile.png"
     },
     {
         "name": "Quattro Formaggi with Chicken",
         "price": 15.90,   
         "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",  
         "category": "Pizza",
-        "image": "./assets/img/pizza_quattro_formaggi_chicken.png"
+        "image": "./assets/img/pizza_quattro_formaggi_chicken.png",
+        "imageMobile": "./assets/img/pizza_quattro_formaggi_chicken_mobile.png"
     },
     {
         "name": "Warm beef arugula salad",
         "price": 16.90,   
         "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",  
         "category": "Salad",
-        "image": "./assets/img/warm_beef_arugula_salad.png" 
+        "image": "./assets/img/warm_beef_arugula_salad.png",
+        "imageMobile": "./assets/img/warm_beef_arugula_salad_mobile.png" 
     },
     {
         "name": "Mini green Salad",
         "price": 7.90,   
         "description": "Green salad, Cucumber, Carrots, Parsley, Radishes ",  
         "category": "Salad",
-        "image": "./assets/img/mini_green_salad.png" 
+        "image": "./assets/img/mini_green_salad.png", 
+        "imageMobile": "./assets/img/mini_green_salad_mobile.png"
     },
     {
         "name": "Green Salad with sea food",
         "price": 16.90,   
         "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",  
         "category": "Salad",
-        "image": "./assets/img/green_salad_sea_food.png"
+        "image": "./assets/img/green_salad_sea_food.png",
+        "imageMobile": "./assets/img/green_salad_with_sea_food_mobile.png"
     },
     {
         "name": "Vegan green salad with tofu",
         "price": 14.90,   
         "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",  
         "category": "Salad",
-        "image": "./assets/img/vegan_green_salad_tofu.png"
+        "image": "./assets/img/vegan_green_salad_tofu.png",
+        "imageMobile": "./assets/img/vegan_green_salad_with_tofu_mobile.png"
     }
 ];
 
@@ -120,7 +132,8 @@ function getMenuTemplate(index) {
 
     return `
       <div class="container-menu">
-        <img class="menu-image" src="${menu.image}" alt="${menu.name}">
+        <img class="menu-image" src="${menu.image}" alt="${menu.name}"> 
+        <img class="menu-image-mobile" src="${menu.imageMobile}" alt="${menu.name}"> 
         <div class="title-and-description">
             <h4 class="menu-title">${menu.name}</h4>
             <p class="menu-description">${menu.description}</p>
